e1677324b5ee42c4e5deb5a0a72b0a786204a341
fanyao <fanyao@manqian.cn>
2017-11-20 16:07:02 +0800

    大修改

---
+++ MQAlertView/MQAlertView.h
@@ -0,0 +1,19 @@
+//
+//  MQAlertView.h
+//  MQAlertView
+//
+//  Created by fanyao on 2017/11/20.
+//  Copyright © 2017年 fanyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MQAlertView.
+FOUNDATION_EXPORT double MQAlertViewVersionNumber;
+
+//! Project version string for MQAlertView.
+FOUNDATION_EXPORT const unsigned char MQAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MQAlertView/PublicHeader.h>
+
+
--- MQAlertViewTests/MQAlertViewTests.swift
+++ MQAlertViewTests/MQAlertViewTests.swift
@@ -1,8 +1,15 @@
-import UIKit
-import XCTest
-import MQAlertView
+//
+//  MQAlertViewTests.swift
+//  MQAlertViewTests
+//
+//  Created by fanyao on 2017/11/20.
+//  Copyright © 2017年 fanyao. All rights reserved.
+//
 
-class Tests: XCTestCase {
+import XCTest
+@testable import MQAlertView
+
+class MQAlertViewTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -16,12 +23,12 @@ class Tests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measure() {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
